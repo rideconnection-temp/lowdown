5503c20d293cb18dbd2fafeb3f090fd0b04c9c4d
Kevin Chambers <kc@kevinchambers.com>
2010-12-07 12:46:40 -0800

    Uncommented previously commented crucial line of code

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ActiveRecord::Base
   extend ActiveSupport::Memoizable
 
-  #point_in_time
+  point_in_time
   belongs_to :pickup_address, :class_name => "Address", :foreign_key => "pickup_address_id"
   belongs_to :dropoff_address, :class_name => "Address", :foreign_key => "dropoff_address_id"
   belongs_to :provider
