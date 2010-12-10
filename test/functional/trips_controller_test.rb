8caa03db00bf0189c2d00494521d21eb90445215
David Turner <novalis@openplans.org>
2010-12-10 15:39:27 -0500

    initial work on functional testing

---
+++ test/functional/trips_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+require 'capybara/rails'
+require 'capybara/dsl'
+
+class TripsControllerTest < ActionController::TestCase
+  include Capybara
+
+  test "should accept imports" do
+    Capybara.default_selector = :xpath
+
+    visit "/trips/import"
+
+    dir = File.dirname(__FILE__)
+    csv = File.join(dir, 'sample.csv')
+    attach_file('file-import', csv)
+    click_button('Import')
+
+    visit "/trips/list"
+
+    assert page.has_selector?('//div[@id="flash"]')
+
+    assert page.find('//h2[@id="result-count"]').text =~ /0 trips/
+
+    select('September', :from => 'query[start_date(2i)]')
+    select('September', :from => 'query[end_date(2i)]')
+    select('1', :from => 'query[start_date(3i)]')
+    select('1', :from => 'query[end_date(3i)]')
+
+    click_button("Search")
+    assert page.find('//h2[@id="result-count"]').text =~ /1 trip/
+  end
+end
