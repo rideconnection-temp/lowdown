659489e83e1f2430468aa8002fb03c8d33186cc8
David Turner <novalis@openplans.org>
2011-01-13 13:06:14 -0500

    fix a number of bugs around summaries

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -12,18 +12,23 @@ class SummariesController < ApplicationController
   end
 
   def create
-    @summary = Summary.new(params[:summary])
-    @summary.save ? redirect_to(:action=>:show_update, :id=>@summary.id) : render(:action => :show_create)
+    @summary = Summary.create(params[:summary]) 
+    if @summary
+      redirect_to(:action=>:show_update, :id=>@summary.id)
+    else
+      render(:action => :show_create)
+    end
   end
 
   def show_update
     @summary = Summary.current_versions.find params[:id]
+    @summary.summary_row.build
     @providers = Provider.all
     @allocations = Allocation.all
   end
 
   def update
-    @summary = Summary.current_versions.find(params[:id])
+    @summary = Summary.current_versions.find(params[:summary][:id])
     @summary.update_attributes(params[:summary]) ?
       redirect_to(:action=>:show_update, :id=>@summary) : render(:action => :show_update)
   end
