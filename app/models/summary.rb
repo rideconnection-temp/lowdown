d6ab33ef41192e7b835c6a07e644050a528b89dd
David Turner <novalis@openplans.org>
2011-01-20 15:17:02 -0500

    rename summary_rows back

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,8 +1,8 @@
 class Summary < ActiveRecord::Base
   point_in_time
-  has_many :summary_row, :order=>'purpose'
+  has_many :summary_rows, :order=>'purpose'
   belongs_to :provider
 
-  accepts_nested_attributes_for :summary_row, :allow_destroy => true, :reject_if => :all_blank
+  accepts_nested_attributes_for :summary_rows, :allow_destroy => true, :reject_if => :all_blank
 
 end
