4c8f89a2bf07ca0187c9290d615f378d74ddb1ae
David Turner <novalis@openplans.org>
2011-01-24 17:15:15 -0500

    fix bogus migration dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120121105500) do
+ActiveRecord::Schema.define(:version => 20110121105500) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "routematch_address_id"
