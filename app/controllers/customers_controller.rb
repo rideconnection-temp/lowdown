46849acceda1b0ed00127463f21388ff7c753450
David Turner <novalis@openplans.org>
2011-02-16 15:02:38 -0500

    finish up spd reports

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,16 +9,56 @@ class CustomersController < ApplicationController
   def report
     @start_date = Date.parse(params[:date])
     @end_date = @start_date.next_month
-    @customers = Customer.find_by_sql(["select * from customers where id in (select customer_id from trips where trips.date between ? and ?)", @start_date, @end_date])
 
-    @wc_approved_rides = Trip.joins(:customer).where("customers.mobility <> 'Ambulatory' and customers.mobility <> 'Unknown' and date between ? and ? ", @start_date, @end_date).count
+    trips = Trip.joins(:allocation=>:project).where("date between ? and ? and valid_end = ? and funding_source='SPD'", @start_date, @end_date, Trip.end_of_time)
 
-    @nonwc_approved_rides = Trip.joins(:customer).where("customers.mobility = 'Ambulatory' and date between ? and ? ", @start_date, @end_date).count
+    @customer_rows = {}
+    @approved_rides = 0
+    @wc_billed_rides = @nonwc_billed_rides = @unknown_billed_rides = 0
+    @wc_mileage = @nonwc_mileage = @unknown_mileage = 0
 
-    @wc_billed_rides = @nonwc_billed_rides = 0 #FIXME
-    @wc_mileage = Trip.joins(:customer).where("customers.mobility <> 'Ambulatory' and customers.mobility <> 'Unknown' and date between ? and ? ", @start_date, @end_date).sum("apportioned_mileage")
-    @nonwc_mileage = Trip.joins(:customer).where("customers.mobility = 'Ambulatory' and date between ? and ? ", @start_date, @end_date).sum("apportioned_mileage")
+    for trip in trips
+      if trip.mobility == "Ambulatory"
+        wheelchair = false
+      elsif trip.mobility == "Unknown"
+        wheelchair = "unknown"
+      else
+        wheelchair = true
+      end
+
+      key = [trip.customer_id, wheelchair]
+      customer = trip.customer
+      row = @customer_rows[key]
+      if row.nil?
+        row = {:customer => customer,
+               :billed_rides=>0, :billable_mileage=>0, :mobility=>wheelchair}
+        @customer_rows[key] = row
+      end
+
+      row[:billed_rides] += 1
+      mileage = trip.estimated_trip_distance_in_miles
+      if mileage < 5
+        rounded_mileage = 0
+      elsif mileage < 25
+        rounded_mileage = mileage - 5
+      else
+        rounded_mileage = 20
+      end
+
+      row[:billable_mileage] += rounded_mileage
+
+      @approved_rides += customer.approved_rides.to_i
+      if wheelchair == "unknown"
+        @unknown_billed_rides += 1
+        @unknown_mileage += rounded_mileage
+      elsif wheelchair
+        @wc_billed_rides += 1
+        @wc_mileage += rounded_mileage
+      else
+        @nonwc_billed_rides += 1
+        @nonwc_mileage += rounded_mileage
+      end
+    end
 
   end
-
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,19 +10,4 @@ class Customer < ActiveRecord::Base
   def name
     return "#{first_name} #{middle_initial} #{last_name}"
   end
-
-  def approved_rides(start_date, end_date)
-    return Trip.where("customer_id = ? and date between ? and ?", id, start_date, end_date).count
-  end
-
-  def billed_rides(start_date, end_date)
-    #FiXME
-    return Trip.where("customer_id = ? and date between ? and ?", id, start_date, end_date).count
-  end
-
-  def billable_mileage(start_date, end_date)
-    #FIXME
-    return Trip.where("customer_id = ? and date between ? and ?", id, start_date, end_date).sum("apportioned_mileage")
-  end
-
 end
