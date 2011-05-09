4058c7bb9a989a2d6580af188ddc9d94ace042f3
Andy Cochran <acochran@openplans.org>
2011-05-09 19:20:02 +0000

    Trips page: remove header when number of trips found is 0

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,24 +15,41 @@ $(document).ready(function() {
       dateFormat: 'yy-mm-dd' 
   });
 
-  $('th.rotate-270').each(function() {
-    var innerSpan = $(this).find('span').get(0);
-
-    var width = null;
-    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
-      width = $(innerSpan).width();
-    }
-    if(width !== null) {
-      $(this).css('height', width);
-    }
-
-    var height = null;
-    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
-      height = $(innerSpan).height();
-    }
-    if(height !== null) {
-      $(this).css('width', height);
-    }
+  // add time picker functionality
+  // http://trentrichardson.com/examples/timepicker/
+  $('.datetimepicker').datetimepicker({
+  	ampm: false,
+  	hourMin: 8,
+  	hourMax: 18,
+		hourGrid: 3,
+  	minuteGrid: 15,
+		timeFormat: 'hh:mm:ss',
+		dateFormat: 'yy-mm-dd',
+    showOn: "button",
+    buttonImage: "/stylesheets/images/calendar-clock.png",
+    buttonImageOnly: true,
+    constrainInput: false
   });
+
+
+//  $('th.rotate-270').each(function() {
+//    var innerSpan = $(this).find('span').get(0);
+//
+//    var width = null;
+//    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
+//      width = $(innerSpan).width();
+//    }
+//    if(width !== null) {
+//      $(this).css('height', width);
+//    }
+//
+//    var height = null;
+//    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
+//      height = $(innerSpan).height();
+//    }
+//    if(height !== null) {
+//      $(this).css('width', height);
+//    }
+//  });
     
 });
