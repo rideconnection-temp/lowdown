4c2cb7e6fc7d02cf1ec78123b9b90cc56ba1ac7e
Andy Cochran <acochran@openplans.org>
2011-05-10 14:37:50 +0000

    fix hard-coded menu links

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,6 +15,43 @@ $(document).ready(function() {
       dateFormat: 'yy-mm-dd' 
   });
 
+  var ISODateFormatToDateObject = function(str) {
+    if(str === null) {
+        return null;
+    }
+
+    var parts = str.split(' ');
+    if(parts.length < 2) {
+      return null;
+    }
+    
+    var dateParts = parts[0].split('-'),
+    timeSubParts = parts[1].split(':'),
+    timeSecParts = timeSubParts[2].split('.'),
+    timeHours = Number(timeSubParts[0]);
+
+    _date = new Date();
+    _date.setFullYear(Number(dateParts[0]));
+    _date.setMonth(Number(dateParts[1])-1);
+    _date.setDate(Number(dateParts[2]));
+    _date.setHours(Number(timeHours));
+    _date.setMinutes(Number(timeSubParts[1]));
+    _date.setSeconds(Number(timeSecParts[0]));
+    if (timeSecParts[1]) {
+        _date.setMilliseconds(Number(timeSecParts[1]));
+    }
+
+    return _date;
+  };
+
+  $('#query_start_date').change(function() {
+    var pickupTimeDate = ISODateFormatToDateObject($('#query_start_date').attr("value"));
+    var appointmentTimeDate = ISODateFormatToDateObject($('#query_end_date').attr("value"));
+    var newPickupDate = new Date(pickupTimeDate.getTime() + (1000 * 60 * 30));    
+    $('#query_end_date').attr( "value", newPickupDate.format("yyyy-mm-dd HH:MM:ss"));
+  });
+
+
   // add time picker functionality
   // http://trentrichardson.com/examples/timepicker/
   $('.datetimepicker').datetimepicker({
@@ -31,33 +68,12 @@ $(document).ready(function() {
     constrainInput: false
   });
 
+
+  // format report headers
   $('th.wrap').each(function(index, element) {
     var th = $(element), word_array, last_word, first_part;
     word_array = th.html().split(/\s+/); // split on spaces
     th.html(word_array.join('<br />')); // join 'em back together with line breaks
   });
-
-
-
-
-//  $('th.rotate-270').each(function() {
-//    var innerSpan = $(this).find('span').get(0);
-//
-//    var width = null;
-//    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
-//      width = $(innerSpan).width();
-//    }
-//    if(width !== null) {
-//      $(this).css('height', width);
-//    }
-//
-//    var height = null;
-//    if(typeof innerSpan !== 'undefined' && innerSpan !== null) {
-//      height = $(innerSpan).height();
-//    }
-//    if(height !== null) {
-//      $(this).css('width', height);
-//    }
-//  });
     
 });
