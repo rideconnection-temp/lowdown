9c3ff5df1c5638762182e938dab0e2c45f924f20
Andy Cochran <acochran@openplans.org>
2011-05-10 18:36:45 +0000

    ui fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,41 +15,41 @@ $(document).ready(function() {
       dateFormat: 'yy-mm-dd' 
   });
 
-  var ISODateFormatToDateObject = function(str) {
-    if(str === null) {
-        return null;
-    }
+//  var ISODateFormatToDateObject = function(str) {
+//    if(str === null) {
+//        return null;
+//    }
+//
+//    var parts = str.split(' ');
+//    if(parts.length < 2) {
+//      return null;
+//    }
+//    
+//    var dateParts = parts[0].split('-'),
+//    timeSubParts = parts[1].split(':'),
+//    timeSecParts = timeSubParts[2].split('.'),
+//    timeHours = Number(timeSubParts[0]);
+//
+//    _date = new Date();
+//    _date.setFullYear(Number(dateParts[0]));
+//    _date.setMonth(Number(dateParts[1])-1);
+//    _date.setDate(Number(dateParts[2]));
+//    _date.setHours(Number(timeHours));
+//    _date.setMinutes(Number(timeSubParts[1]));
+//    _date.setSeconds(Number(timeSecParts[0]));
+//    if (timeSecParts[1]) {
+//        _date.setMilliseconds(Number(timeSecParts[1]));
+//    }
+//
+//    return _date;
+//  };
 
-    var parts = str.split(' ');
-    if(parts.length < 2) {
-      return null;
-    }
-    
-    var dateParts = parts[0].split('-'),
-    timeSubParts = parts[1].split(':'),
-    timeSecParts = timeSubParts[2].split('.'),
-    timeHours = Number(timeSubParts[0]);
-
-    _date = new Date();
-    _date.setFullYear(Number(dateParts[0]));
-    _date.setMonth(Number(dateParts[1])-1);
-    _date.setDate(Number(dateParts[2]));
-    _date.setHours(Number(timeHours));
-    _date.setMinutes(Number(timeSubParts[1]));
-    _date.setSeconds(Number(timeSecParts[0]));
-    if (timeSecParts[1]) {
-        _date.setMilliseconds(Number(timeSecParts[1]));
-    }
-
-    return _date;
-  };
-
-  $('#query_start_date').change(function() {
-    var pickupTimeDate = ISODateFormatToDateObject($('#query_start_date').attr("value"));
-    var appointmentTimeDate = ISODateFormatToDateObject($('#query_end_date').attr("value"));
-    var newPickupDate = new Date(pickupTimeDate.getTime() + (1000 * 60 * 30));    
-    $('#query_end_date').attr( "value", newPickupDate.format("yyyy-mm-dd HH:MM:ss"));
-  });
+//  $('#query_start_date').change(function() {
+//    var pickupTimeDate = ISODateFormatToDateObject($('#query_start_date').attr("value"));
+//    var appointmentTimeDate = ISODateFormatToDateObject($('#query_end_date').attr("value"));
+//    var newPickupDate = new Date(pickupTimeDate.getTime() + (1000 * 60 * 30));    
+//    $('#query_end_date').attr( "value", newPickupDate.format("yyyy-mm-dd HH:MM:ss"));
+//  });
 
 
   // add time picker functionality
@@ -63,7 +63,7 @@ $(document).ready(function() {
 		timeFormat: 'hh:mm:ss',
 		dateFormat: 'yy-mm-dd',
     showOn: "button",
-    buttonImage: "/stylesheets/images/calendar-clock.png",
+    buttonImage: "../stylesheets/images/calendar-clock.png",
     buttonImageOnly: true,
     constrainInput: false
   });
