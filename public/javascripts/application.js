682cb86d37b3b2584347034752fd0cefea9105ce
Andy Cochran <acochran@openplans.org>
2011-05-12 19:23:21 +0000

    make datepicker trigger use image with path relative to style.css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,66 +8,12 @@ $(document).ready(function() {
       return false;
   });
 
+  // date picker
   $('.datepicker').datepicker({
       showOn: "button",
-      buttonImage: "../stylesheets/images/calendar.png",
-      buttonImageOnly: true,
+      buttonText: "Select",
       dateFormat: 'yy-mm-dd' 
   });
-
-//  var ISODateFormatToDateObject = function(str) {
-//    if(str === null) {
-//        return null;
-//    }
-//
-//    var parts = str.split(' ');
-//    if(parts.length < 2) {
-//      return null;
-//    }
-//    
-//    var dateParts = parts[0].split('-'),
-//    timeSubParts = parts[1].split(':'),
-//    timeSecParts = timeSubParts[2].split('.'),
-//    timeHours = Number(timeSubParts[0]);
-//
-//    _date = new Date();
-//    _date.setFullYear(Number(dateParts[0]));
-//    _date.setMonth(Number(dateParts[1])-1);
-//    _date.setDate(Number(dateParts[2]));
-//    _date.setHours(Number(timeHours));
-//    _date.setMinutes(Number(timeSubParts[1]));
-//    _date.setSeconds(Number(timeSecParts[0]));
-//    if (timeSecParts[1]) {
-//        _date.setMilliseconds(Number(timeSecParts[1]));
-//    }
-//
-//    return _date;
-//  };
-
-//  $('#query_start_date').change(function() {
-//    var pickupTimeDate = ISODateFormatToDateObject($('#query_start_date').attr("value"));
-//    var appointmentTimeDate = ISODateFormatToDateObject($('#query_end_date').attr("value"));
-//    var newPickupDate = new Date(pickupTimeDate.getTime() + (1000 * 60 * 30));    
-//    $('#query_end_date').attr( "value", newPickupDate.format("yyyy-mm-dd HH:MM:ss"));
-//  });
-
-
-  // add time picker functionality
-  // http://trentrichardson.com/examples/timepicker/
-  $('.datetimepicker').datetimepicker({
-  	ampm: false,
-  	hourMin: 8,
-  	hourMax: 18,
-		hourGrid: 3,
-  	minuteGrid: 15,
-		timeFormat: 'hh:mm:ss',
-		dateFormat: 'yy-mm-dd',
-    showOn: "button",
-    buttonImage: "../stylesheets/images/calendar-clock.png",
-    buttonImageOnly: true,
-    constrainInput: false
-  });
-
 
   // format report headers
   $('th.wrap').each(function(index, element) {
