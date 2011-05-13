754f6a63f2afec5947dc6a2a3c89bbf634940e51
Andy Cochran <acochran@openplans.org>
2011-05-13 18:53:33 +0000

    style and markup changes; jeff's .js changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,5 +21,63 @@ $(document).ready(function() {
     word_array = th.html().split(/\s+/); // split on spaces
     th.html(word_array.join('<br />')); // join 'em back together with line breaks
   });
+
+  // generates a new group by select value, given each of the custom field values
+  var updateCustomOptionValue = function() {
+    var realSelect = $("#group-by");
+
+    var newRealSelectValue = "";
+    var customSelectValues = $("#group-by-custom-wrapper").find("select");
+
+    customSelectValues.each(function() {
+	var value = $(this).find("option:selected").val();
+
+	if(newRealSelectValue.length > 0) {
+	  newRealSelectValue += ",";
+	}
+	newRealSelectValue += value;
+    });
+
+    realSelect.find("option:selected").val(newRealSelectValue);
+  };
+
+  // shows the custom grouping UI when the "custom" option is chosen
+  $('#group-by').change(function() {
+    var selectedLabel = $(this).find("option:selected").text();
     
+    if(selectedLabel === "Define Custom Grouping...") {
+	$("#group-by-custom-section").show();
+	$("#group-by-custom-section").find("select").change(updateCustomOptionValue).trigger("change");
+    } else {
+	$("#group-by-custom-section").hide();
+    }
+  });
+  $('#group-by').trigger("change");
+    
+  // duplicate the "template" span, and add a remove button to the new option item when user chooses to 
+  // add new grouping field to the custom option
+  $('#group-by-custom-section').find("a").click(function() {
+    var wrapper = $("#group-by-custom-wrapper");
+    var template = wrapper.find(".template");
+
+    var newOption = template.clone();
+    newOption.removeClass("template");
+
+    // add remove link to new option span
+    var newOptionRemoveLink = $("<a>Remove</a>").click(function() {
+	$(this).parent().remove();
+        updateCustomOptionValue();
+    });
+
+    newOption.append(newOptionRemoveLink);
+
+    // bind new listener to select to update real select's value when this one is changed
+    newOption.find("select").change(updateCustomOptionValue);
+
+    wrapper.append(newOption);
+    updateCustomOptionValue();
+
+    return false;
+  });
+
 });
