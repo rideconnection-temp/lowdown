311fe161f73360567b559fec5ab7017431ca6457
Kevin Chambers <kc@kevinchambers.com>
2012-06-19 19:03:11 -0700

    Fix bug in versioning of summary rows

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -100,18 +100,18 @@ class SummariesController < ApplicationController
   def update
     old_version = Summary.find(params[:summary][:id])
     @summary = old_version.current_version
-    prev = @summary.previous
 
     @providers = Provider.with_summary_data.order(:name).all
     @versions = @summary.versions.reverse
 
     #gather up the old row objects
-    old_rows = @summary.summary_rows.map &:clone if prev
+    old_rows = @summary.summary_rows.map &:clone 
 
     @summary.attributes = params[:summary]
     if @summary.save
       #this created a new prior version, to which we want to reassign the
       #newly-created old-valued summary rows
+      prev = @summary.previous
       if prev
         for row in old_rows
           row.summary_id=prev.id
