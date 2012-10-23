rideconnection-temp/lowdown

commit 2ad823919c7a934ff2e5f841136177ae75869ed9

Flex reports: fix query for collecting summary trip data.

app/models/report_row.rb
@@ -282,9 +282,8 @@ start_date, end_date, end_date ]))
   end
 
   def collect_trips_by_summary(allocation, start_date, end_date, pending=false, adjustment=false)
-    results = Summary.select("sum(in_district_trips) as in_district_trips, sum(out_of_district_trips) as out_of_district_trips, turn_downs, unduplicated_riders as undup_riders")
-    results = results.where(:allocation_id => allocation['id'])
-    results = results.joins(:summary_rows).group("turn_downs, summaries.unduplicated_riders")
+    results = Summary.select("sum(in_district_trips) as in_district_trips, sum(out_of_district_trips) as out_of_district_trips, SUM(turn_downs) AS turn_downs, SUM(unduplicated_riders) as undup_riders")
+    results = results.where(:allocation_id => allocation['id']).joins(:summary_rows)
     results = results.data_entry_complete unless pending
 
     if adjustment && false # turn off adjustments option for now

commit 58e5b7fa4c2f2a26c2dbf9a5fd3140356d0d567c

Flex Reports: Fix overcounting of turndowns
This fixes a new bug introduced in
2ad823919c7a934ff2e5f841136177ae75869ed9.
Rather than attempt to sum across a one-to-many join, best split out into two
separate queries.

app/models/report_row.rb
@@ -282,12 +282,22 @@ start_date, end_date, end_date ]))
   end
 
   def collect_trips_by_summary(allocation, start_date, end_date, pending=false, adjustment=false)
-    results = Summary.select("sum(in_district_trips) as in_district_trips, sum(out_of_district_trips) as out_of_district_trips, SUM(turn_downs) AS turn_downs, SUM(unduplicated_riders) as undup_riders")
+    results = Summary.select("sum(in_district_trips) as in_district_trips, sum(out_of_district_trips) as out_of_district_trips")
     results = results.where(:allocation_id => allocation['id']).joins(:summary_rows)
     results = results.data_entry_complete unless pending
-
     if adjustment && false # turn off adjustments option for now
-      add_results, subtract_results = collect_adjustment_by_summary(sql + group_by, allocation, start_date, end_date)
+      add_results, subtract_results = collect_adjustment_by_summary(sql, allocation, start_date, end_date)
+    else
+      add_results = results.current_versions.date_range(start_date, end_date).first.try(:attributes)
+      subtract_results = {}
+    end
+    apply_results(add_results, subtract_results)
+
+    results = Summary.select("SUM(turn_downs) AS turn_downs, SUM(unduplicated_riders) as undup_riders")
+    results = results.where(:allocation_id => allocation['id'])
+    results = results.data_entry_complete unless pending
+    if adjustment && false # turn off adjustments option for now
+      add_results, subtract_results = collect_adjustment_by_summary(sql, allocation, start_date, end_date)
     else
       add_results = results.current_versions.date_range(start_date, end_date).first.try(:attributes)
       subtract_results = {}