0b05c7dd819dcccd53ae309dfc917d1770a4ad2d
Kevin Chambers <KChambers@RideConnection.org>
2013-01-05 21:11:40 -0800

    Move secret token
    Take secret token for securing cookies from repository, and
    put in app_config.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lowdown::Application.config.secret_token = 'ce2a6e22e2ea9e32970c796f37c3479ac5e4891647e2bac5995e2c0a89a171f821d0597081dc052e6a2b8753f284f2979d2920e1fcc6b860ed7c35adf6d362b3'
+Lowdown::Application.config.secret_token = APP_CONFIG[:secret_token]
