14c3da2532d45ea98e75d7b75261c41f45940aff
Kevin Chambers <kchambers@rideconnection.org>
2013-04-01 10:35:56 -0700

    A little link cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
 
   def require_admin_user
     if !user_signed_in? || !current_user.is_admin
-      return redirect_to "/"
+      return redirect_to root_url
     end
   end
 
   def test_exception_notification
-      raise 'Testing, 1 2 3.'
+    raise 'Testing, 1 2 3.'
   end
 
   private 
