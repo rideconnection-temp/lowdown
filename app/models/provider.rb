58b410d321dcb7c2be7db644a7bd507096742e32
Kevin Chambers <kchambers@rideconnection.org>
2013-04-03 09:45:24 -0700

    Remove unnecessary scope in allocations form

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -14,7 +14,6 @@ class Provider < ActiveRecord::Base
   scope :with_trip_data, where("id in (SELECT provider_id FROM allocations WHERE trip_collection_method = 'trips' or run_collection_method = 'trips' or cost_collection_method = 'trips')")
   scope :for_multnomah_ads, where("id in (SELECT provider_id FROM allocations WHERE project_id = (SELECT id FROM projects WHERE funding_source = ?))",'Multnomah ADS')
   scope :partners, where(:provider_type => "Partner")
-  scope :partners_or_current, lambda{|provider_id| where(["provider_type = ? OR id = ?", "Partner", provider_id])}
   scope :reporting_agencies, where("id in (SELECT reporting_agency_id from allocations)")
   scope :providers_in_allocations, where("id in (SELECT provider_id from allocations)")
   scope :default_order, order(:name)
