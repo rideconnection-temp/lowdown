98a0d4df23c61a5df5d939aa6bba870a19f135d1
Kevin Chambers <kc@kevinchambers.com>
2014-02-16 23:52:37 -0800

    Fix exception notification config file

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
-if Rails.env.production?
+if Rails.env.production? || true
   #Exception Notification
-  Whatever::Application.config.middleware.use ExceptionNotification::Rack,
+  Lowdown::Application.config.middleware.use ExceptionNotification::Rack,
     :email => {
       :email_prefix => APP_CONFIG[:exception_email_prefix],
       :sender_address => APP_CONFIG[:exception_sender_address],
