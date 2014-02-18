e20afc64b9fc664c05d35c952997683859f4b82b
Kevin Chambers <kchambers@rideconnection.org>
2014-02-18 15:25:06 -0800

    Add cache-busting headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper :all
-  before_filter :authenticate_user!, :allow_active_users_only
+  before_filter :authenticate_user!, :allow_active_users_only, :set_cache_buster
+
+  def test_exception_notification
+    raise 'Testing, 1 2 3.'
+  end
+
+  private 
+
+  def has_real_changes?(record)
+    record.changes.values.each {|change| return true unless change[0].blank? && change[1].blank? }
+    return false
+  end
+
+  def require_admin_user
+    if !user_signed_in? || !current_user.is_admin
+      return redirect_to root_url
+    end
+  end
 
   def allow_active_users_only
     if user_signed_in? && !current_user.active?
@@ -16,20 +33,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def require_admin_user
-    if !user_signed_in? || !current_user.is_admin
-      return redirect_to root_url
-    end
-  end
-
-  def test_exception_notification
-    raise 'Testing, 1 2 3.'
-  end
-
-  private 
-
-  def has_real_changes?(record)
-    record.changes.values.each {|change| return true unless change[0].blank? && change[1].blank? }
-    return false
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
