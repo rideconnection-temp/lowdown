fcb02ce0f7d32caf26ce4b02b97f04031e997bd1
Kevin Chambers <kchambers@rideconnection.org>
2014-02-27 16:37:15 -0800

    Add routing for trips

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Lowdown::Application.routes.draw do
   resources :projects
   resources :programs
   resources :runs
+  resources :trips
   resources :trimet_providers
   resources :trimet_programs
   resources :trimet_report_groups
