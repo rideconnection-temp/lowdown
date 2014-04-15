93956810d60a5ca3e804a6f4f047f8e8bd7b9475
Kevin Chambers <kchambers@rideconnection.org>
2014-04-15 12:22:23 -0700

    Update routes file
    remove "match" with proper verbs

--- config/routes.rb
+++ config/routes.rb
@@ -51,21 +51,21 @@ Lowdown::Application.routes.draw do
   resources :customers, only: [:show, :update]
 
   devise_for :users, controllers: {sessions: "users"} do
-    get "new_user" => "users#new_user"
-    get "users/show_create" => "users#show_create"
+    get  "new_user" => "users#new_user"
+    get  "users/show_create" => "users#show_create"
     post "users/create_user" => "users#create_user"
-    get "init" => "users#show_init"
+    get  "init" => "users#show_init"
     post "init" => "users#init"
-    post "logout", to: "users#sign_out"
-    get "users/index" => "users#index"
-    get "users/show_change_password" => "users#show_change_password"
-    match "users/change_password"  => "users#change_password"
+    post "logout" => "users#sign_out"
+    get  "users/index" => "users#index"
+    get  "users/show_change_password" => "users#show_change_password"
+    post "users/change_password"  => "users#change_password"
     post "users/update" => "users#update"
   end
 
   root to: 'dashboard#index'
 
-  match "test_exception_notification" => "application#test_exception_notification"
+  get "test_exception_notification" => "application#test_exception_notification"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
