119bd85afbfb1ba637d75726633e9da237d008fc
Kevin Chambers <kchambers@rideconnection.org>
2014-04-30 16:12:09 -0700

    Update customer controller with strong params

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,10 +10,33 @@ class CustomersController < ApplicationController
   def update
     @customer = Customer.find params[:id]
     
-    if @customer.update_attributes params[:customer]
+    if @customer.update_attributes safe_params
       redirect_to @customer, notice: 'Customer was successfully updated.'
     else
       render :show
     end
   end
+
+  private
+
+    def safe_params
+      params.require(:customer).permit(
+        :last_name,
+        :first_name,
+        :middle_initial,
+        :birthdate,
+        :sex,
+        :race,
+        :veteran_status,
+        :customer_type,
+        :mobility,
+        :email,
+        :telephone_primary,
+        :telephone_primary_extension,
+        :telephone_secondary,
+        :telephone_secondary_extension,
+        :language_preference,
+        :prime_number
+      )
+    end
 end
